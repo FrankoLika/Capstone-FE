2faa4df020855749e62452791b9fe4d94bd055cf
Lika Franko <likafranko01@gmail.com>
2023-07-05 18:01:48 +0200

    react toast for login notifications

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,17 @@
 import React, { useState } from 'react'
 import '../styles/Login.css'
 import { Form, FormControl } from 'react-bootstrap'
+import { Toaster } from 'react-hot-toast'
+import { Toast } from '../utilities/notifications'
+import { useNavigate } from 'react-router-dom'
+
 const Login = () => {
+    const navigate = useNavigate();
+
     const [formData, setFormData] = useState({})
+
+    const successToast = new Toast("Login effettuato con successo!")
+    const errorToast = new Toast("Login fallito")
 
     const login = async (e) => {
         e.preventDefault();
@@ -17,13 +26,21 @@ const Login = () => {
             const data = await req.json()
             const jwt = data.token
             localStorage.setItem('jwt', jwt);
+            if (jwt) {
+                successToast.success();
+                setTimeout(() => {
+                    navigate('/Homepage', { replace: true });
+                }, 1000);
+            } else {
+                errorToast.warning();
+            }
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     }
     return (
         <div className=' h-100 d-flex align-items-center justify-content-center'>
-            <div className='h-75 w-25 bg-white shadow rounded'>
+            <div className='h-75 w-25 bg-white shadow rounded square border border-warning'>
                 <h1 className='text-center mt-3 fst-italic fw-bold'>PostVerse</h1>
                 <Form onSubmit={login} className='m-3'>
                     <FormControl
@@ -51,6 +68,7 @@ const Login = () => {
                     </button>
                 </Form>
                 <hr></hr>
+                <Toaster />
             </div>
         </div>
     )
