e086256e80e653aead5136614edb6b6837d9d07a
Lika Franko <likafranko01@gmail.com>
2023-07-05 17:17:15 +0200

    protected routes

---
+++ src/middleware/ProtectedRoutes.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import jwtDecode from "jwt-decode";
+import Login from "../pages/Login";
+const useAuth = () => {
+    return localStorage.getItem("jwt")
+}
+
+const useSession = () => {
+    const session = useAuth()
+    const decodedSession = session ? jwtDecode(session) : null
+    const navigate = useNavigate()
+    useEffect(() => {
+        if (!session) {
+            navigate('/', {replace : true})
+        }
+    }, [navigate, session])
+    return decodedSession
+}
+
+export default function ProtectedRoutes() {
+    const isAuthorized = useAuth()
+    const session = useSession()
+    return isAuthorized ? <Outlet /> : <Login />
+}
+
+
